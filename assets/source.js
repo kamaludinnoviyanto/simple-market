1b2ff2e08c8f3eac0f0548945c597d8b6c26bf57
NOVIYANTOKAMALUDIN <noviyantokamaludin@gmail.com>
2022-09-06 15:44:04 +0700

    first commit

---
+++ assets/source.js
@@ -0,0 +1,8 @@
+//memanggil tahun pada footer
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+// memanggil onclick buy function
+function buy (nama_produk){
+    alert (nama_produk);
+}
